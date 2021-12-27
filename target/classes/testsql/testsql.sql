45de66da861ead47c13b3895648f50bb635cd69e
sean <csh92321@naver.com>
2021-12-27 14:05:12 +0900

    ignore 수정

--- target/classes/testsql/testsql.sql
+++
@@ -1,7 +0,0 @@
-create table test_meet_board(
-id varchar2(20) primary key not null,
-write_num number not null,
-savedate date default sysdate,
-content varchar2(1000)
-);
-create sequence test_meet_board_seq;
