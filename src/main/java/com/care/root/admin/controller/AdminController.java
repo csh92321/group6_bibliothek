8f20e3182577b29bf4628cf46f69affe43207e0e
ssolly <solly0829@gmail.com>
2022-01-29 21:39:02 +0900

    admin_admin Interceptor 생성 및 적용

--- src/main/java/com/care/root/admin/controller/AdminController.java
+++ src/main/java/com/care/root/admin/controller/AdminController.java
@@ -23,15 +23,7 @@ public class AdminController implements MemberSessionName {
 	@GetMapping("adminPage")
 	public String adminSuccessLogin(HttpSession session) {
 		System.out.println("관리자 페이지 진입");
-		System.out.println(session.getAttribute(LOGIN));
-		if (session.getAttribute(LOGIN)==null) {
-			return "admin/wrongApproach";
-		}
-		else if (session.getAttribute(LOGIN).equals("group6")) {
-			return "admin/adminPage"; 
-		} else {
-			return "admin/wrongApproach";
-		}
+		return "admin/adminPage"; 
 	}
 	
 	@GetMapping("PBlistAll")
---
+++ src/main/java/com/care/root/admin/interceptor/AdminInterceptor.java
@@ -0,0 +1,39 @@
+package com.care.root.admin.interceptor;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.care.root.common.MemberSessionName;
+
+public class AdminInterceptor extends HandlerInterceptorAdapter implements MemberSessionName{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		System.out.println("admin 진입시?sessionID : " + LOGIN);
+		System.out.println(session.getValue(LOGIN));
+		
+		if(session.getAttribute(LOGIN)==null) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('접근 권한이 없습니다');"
+					+"location.href='"+request.getContextPath()+"';</script>");
+					return false;
+		} else if (session.getAttribute(LOGIN).equals("group6")) {
+			return true;
+		} else {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('접근 권한이 없습니다');"
+					+"location.href='"+request.getContextPath()+"';</script>");
+					return false;
+		}
+	}	
+	
+}
